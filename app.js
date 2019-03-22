48be2180e183fa7288e85fa90296a2e8fef29dab
vaxosv <vaxo.sv@gmail.com>
2019-03-22 13:25:33 +0400

    adminka

--- app.js
+++ app.js
@@ -13,4 +13,9 @@ app.get("/", (req, res)=>{
     res.render("main");
 })
 
+app.get("/admin", (req, res) => {
+    res.render("admin");
+})
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
