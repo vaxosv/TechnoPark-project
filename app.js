0cc785288fbc2e214ca33662a6d753b49fc2f243
vaxosv <vaxo.sv@gmail.com>
2019-03-22 13:18:40 +0400

    pug

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 8081;
 
-app.get("/", (req, res) => res.send("Hello World  sadasdsadasdas!"));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use("/public", express.static("public"));
+
+
+app.get("/", (req, res)=>{
+    res.render("main");
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
